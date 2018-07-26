85a4aaa5acbfc2e36a9349c90ba3b8dd6e848bf4
Kevin Fang <kfang@curii.com>
2018-07-26 15:49:23 +0000

    Added CWL scripts for 1 hot encoding

---
+++ just_encode.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+from sklearn.preprocessing import OneHotEncoder
+from sys import argv
+import numpy as np
+
+if not len(argv) == 2:
+	print("Usage: python just_encode.py <arr.npy>")
+
+print("Array loading...")
+arr = np.load(argv[1])
+
+print("Array loaded. Encoding...")
+enc = OneHotEncoder()
+transformed = enc.fit_transform(arr)
+
+print("Encoding finished. Saving as encode.npy")
+np.save("encoded.npy", transformed.toarray())
